a9774e33555789c593808ed03222f62b9490943f
Jesus Samuel benjumea <jsbenjumec@eafit.edu.co>
2025-09-17 17:26:53 +0000

    Preprocess test

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -34,10 +34,10 @@ def preprocess(dataset, normalize=True, expand_dims=True):
 
 def preprocess_and_log(steps):
 
-    with wandb.init(project="MLOps-Pycon2023",name=f"Preprocess Data ExecId-{args.IdExecution}", job_type="preprocess-data") as run:    
+    with wandb.init(project="Project",name=f"Preprocess Data ExecId-{args.IdExecution}", job_type="preprocess-data") as run:    
         processed_data = wandb.Artifact(
             "mnist-preprocess", type="dataset",
-            description="Preprocessed MNIST dataset",
+            description="Preprocessed IRIS dataset",
             metadata=steps)
          
         # ✔️ declare which artifact we'll be using
